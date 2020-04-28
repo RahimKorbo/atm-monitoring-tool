8fa9b91a5034542afbb7d5f11e14cfffa5e33fb3
Rahim <36994391+Rahko@users.noreply.github.com>
2020-04-28 14:33:53 +0530

    Add files via upload

--- src/Routing.js
+++ src/Routing.js
@@ -3,7 +3,10 @@ import { Switch, Route, Link } from "react-router-dom";
 import Login from "./components/common/Login";
 import Dashboard from "./components/admin/Dashboard";
 import BankList from "./components/bank/BankList";
-import AtmList from "./components/ATM/AtmList";
+import AtmList from "./components/atm/AtmList";
+import AtmDetailsPage from "./components/atm/AtmDetailsPage";
+import addAtm from "./components/atm/addAtm";
+import BankDashboard from "./components/bank/BankDashboard";
 
 export function Main() {
   return (
@@ -13,6 +16,11 @@ export function Main() {
         <Route exact path="/dash" component={Dashboard} />
         <Route exact path="/bankList" component={BankList} />
         <Route exact path="/atmList" component={AtmList} />
+        <Route exact path="/atmDetailsPage" component={AtmDetailsPage} />
+        
+        <Route exact path="/addAtm" component={addAtm} />
+        
+        <Route exact path="/bankDashboard" component={BankDashboard} />
       </Switch>
     </main>
   );
